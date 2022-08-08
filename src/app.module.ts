b4275cc384153b256c5e8885fd9ed0c870870411
Jhon Huiracocha <jhonhuiracocha123456789@gmail.com>
2022-08-07 19:11:28 -0500

    added environment variable and validator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
 
+import { validate } from './common/validators';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+
+  const globalPrefix: string = 'api';
+  const port: number = config.get<number>('PORT');
+  const environment: string = config.get<string>('NODE_ENV');
+
+  app.enableCors();
+  app.setGlobalPrefix(globalPrefix);
+
+  await app.listen(port, () => {
+    Logger.log(`🚀 Server ready at http://localhost:${port}/${globalPrefix}`);
+    Logger.log(`Running in ${environment}`);
+  });
 }
 bootstrap();
