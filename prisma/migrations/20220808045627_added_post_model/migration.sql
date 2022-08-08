2fcf76b5ffe64bdb67c2c31503f3cfe846f9951b
Jhon Huiracocha <jhonhuiracocha123456789@gmail.com>
2022-08-07 23:57:21 -0500

    updated schema prisma: added post model

---
+++ prisma/migrations/20220808045627_added_post_model/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE `Post` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(150) NOT NULL,
+    `description` TEXT NULL,
+    `body` TEXT NOT NULL,
+    `thumbnail` TEXT NOT NULL,
+    `status` BOOLEAN NOT NULL DEFAULT true,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+    `authorId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Post_title_key`(`title`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Post` ADD CONSTRAINT `Post_authorId_fkey` FOREIGN KEY (`authorId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
